e42ec0fa1d0fe02a577ebc8251a15e9ea4c70d2a
ABUZER EMRE OSMANOGLU <a.osmanoglu@jandarma.gov.tr>
2025-06-18 09:02:58 +0300

    Admin paneline yeni envanter ve envanter öğesi yönetimi eklendi. Gelir ve gider modellerine minimum değer doğrulayıcıları eklendi. Filtreleme seçenekleri güncellendi ve görüntüleme alanları genişletildi.

--- treasury/admin.py
+++ treasury/admin.py
@@ -1,18 +1,44 @@
 from django.contrib import admin
+from unfold.contrib.filters.admin import (
+    ChoicesCheckboxFilter,
+    RangeDateFilter,
+    RangeNumericFilter,
+)
 
 from core.admin import DefaultAdmin
-from treasury.models import Expense, Income
+from treasury.models import Expense, Income, Inventory, InventoryItem
 
 
 @admin.register(Income)
 class IncomeAdmin(DefaultAdmin):
-    list_filter = ("type", "date")
+    list_filter = [
+        ("type", ChoicesCheckboxFilter),
+        ("date", RangeDateFilter),
+        ("amount", RangeNumericFilter),
+    ]
     list_display = ("type", "user", "amount", "date", "description")
     search_fields = ("user__first_name", "user__last_name", "description")
 
 
 @admin.register(Expense)
 class ExpenseAdmin(DefaultAdmin):
-    list_filter = ("type", "date")
+    list_filter = [
+        ("type", ChoicesCheckboxFilter),
+        ("date", RangeDateFilter),
+        ("amount", RangeNumericFilter),
+    ]
     list_display = ("type", "user", "amount", "date", "description")
     search_fields = ("user__first_name", "user__last_name", "description")
+
+
+@admin.register(Inventory)
+class InventoryAdmin(DefaultAdmin):
+    list_filter = [("quantity", RangeNumericFilter),]
+    list_display = ("item", "user", "quantity", "description")
+    search_fields = ("item__name", "user__first_name", "user__last_name", "description")
+
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(DefaultAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name", "description")
--- treasury/models.py
+++ treasury/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -17,7 +18,9 @@ class Income(BaseModel):
         null=True,
         blank=True,
     )
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
     date = models.DateField(default=timezone.now)
     description = models.TextField(blank=True)
 
@@ -36,9 +39,37 @@ class Expense(BaseModel):
         null=True,
         blank=True,
     )
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
     date = models.DateField(default=timezone.now)
     description = models.TextField()
 
     def __str__(self):
         return self.type
+
+
+class Inventory(BaseModel):
+    item = models.ForeignKey(
+        "InventoryItem",
+        on_delete=models.CASCADE,
+        related_name="inventories",
+    )
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="inventories",
+    )
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.item.name
+
+
+class InventoryItem(BaseModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
